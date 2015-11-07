f9f2340985123bc128980d5d7ffd0960f38bdffc
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-07 08:59:16 +0000

    add_transaction method

--- bank.rb
+++ bank.rb
@@ -1,11 +1,17 @@
 class BankAccount
+  attr_reader :name
+  
   def initialize(name)
     @name = name
     @transactions = []
   end
   
-  
+  def add_transaction(description, amount)
+    @transactions.push(description: description, amount: amount)
+  end
 end
 
 bank_account = BankAccount.new("Richy")
+puts bank_account.inspect
+bank_account.add_transaction("pocket money", 1000)
 puts bank_account.inspect
